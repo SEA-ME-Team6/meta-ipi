8d90e3c377ae35687ac6719297f59d28ffe8701e
jehyngn <wpgud1227@kookmin.ac.kr>
2023-11-15 11:54:26 +0100

    Deleted recipes-test

--- recipes-test/hu-cmake/files/head_unit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR=$(dirname "$0")
-
-export COMMONAPI_CONFIG="$SCRIPT_DIR/head-unit.ini"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCRIPT_DIR/build"
-
-$SCRIPT_DIR/build/head_unit
-
